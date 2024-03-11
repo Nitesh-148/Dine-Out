c1c8a1efda794e5cc80a76f7f9bd6337f2171b35
Shashank Shrivastava <111704026+Nitesh-148@users.noreply.github.com>
2024-03-12 01:34:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Menu from './components/Menu';
@@ -10,9 +9,7 @@ function App() {
   return (
  <>
  <Nav/>
- <BrowserRouter>
  <Main/>
- </BrowserRouter>
  <Menu/>
  <Footer/>
  </>
